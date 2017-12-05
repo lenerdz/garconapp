4404fdfc2e3f3607f224d23892ff56094eced7a3
Nerd <gerardoccf@gmail.com>
2017-12-05 16:45:29 -0300

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$('.collection-item').on('click', function(){
+
+		var $badge = $('.badge', this);
+		if($badge.length == 0) {
+			$badge = $('<span class="badge brown-text">0</span>')
+			.appendTo(this);
+		}
+		$badge.text(parseInt($badge.text()) + 1);
+	});
+
+	$('.modal-trigger').leanModal();
+});
